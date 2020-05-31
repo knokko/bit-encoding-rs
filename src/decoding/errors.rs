4622ed94cb7cbaaba7f4501a6fcbceaa91ccf49c
knokko <knokogator@hotmail.com>
2020-05-31 20:19:05 +0200

    Add missing documentation

--- src/decoding/errors.rs
+++ src/decoding/errors.rs
@@ -1,17 +1,53 @@
 use crate::*;
 
+/// Represents an error that occurred while trying to decode the data read
+/// from a `BitSource`.
+/// 
+/// Most of the errors in this enum indicate programming errors or a corrupted
+/// `BitSource`, except for the `Reading` error, which indicates that an
+/// error occurred during reading data rather than decoding it.
 #[derive(Debug)]
 pub enum DecodeError {
 
+    /// Some methods of `BitDecoder` return a `Vec` of elements (for instance
+    /// a `Vec` of `u32`s). In order to decode such a vector, its length will
+    /// have to be decoded somehow. If that decoded length is very large,
+    /// the decoder would try to allocate a very big `Vec`, which could cause
+    /// the process to run out of memory. 
+    /// 
+    /// To prevent this, some implementations refuse to decode `Vec`s that
+    /// would have a length that is greater than the maximum length they
+    /// allow. If that occurs, this `BitVecLength` error will be returned
+    /// and no attempt will be made to allocate so much memory. 
     BigVecLength(LengthExceeded),
 
+    /// Similarly to `BigVecLength`, some implementations of `BitDecoder`
+    /// also have a maximum length for strings they are willing to decode.
+    /// 
+    /// If that maximum length would be exceeded, this error will be
+    /// returned.
     BigStringLength(LengthExceeded),
 
+    /// Some methods of `BitDecoder` return a `Vec` of elements, for instance
+    /// a `Vec<i16>`. The length of the vector will have to be decoded first. 
+    /// However, the decoded length might be greater than the maximum value 
+    /// of `usize`. If that occurs, this error will be returned.
     VecLengthOverflow{ length: LengthType },
 
+    /// When decoding a `String`, the length of that string will have to be
+    /// decoded first. It is possible that the decoded length is greater than
+    /// the maximum value of `usize`. If that occurs, this error will be
+    /// returned.
     StringLengthOverflow{ length: LengthType },
+
+    /// This error indicates that an error occurred while reading the data
+    /// needed to decode something. 
+    Reading(ReadError)
 }
 
+/// This indicates that some maximum length was exceeded during decoding
+/// something. This struct stores both the maximum length and the
+/// actual/read length. 
 #[derive(Debug)]
 pub struct LengthExceeded {
 
@@ -25,10 +61,13 @@ impl LengthExceeded {
         LengthExceeded { max_length, read_length }
     }
 
+    /// Gets the maximum length the `BitDecoder` was willing to decode
     pub fn get_max_length(&self) -> LengthType {
         self.max_length
     }
 
+    /// Gets the length of the Vec, String (or whatever else) that was
+    /// being decoded
     pub fn get_read_length(&self) -> LengthType {
         self.read_length
     }
--- src/source/mod.rs
+++ src/source/mod.rs
@@ -2,7 +2,25 @@ mod errors;
 
 pub use errors::*;
 
+/// A type from which bools can be read.
+/// 
+/// Typical implementations would read bools from a Vec of bools or binary
+/// integers, but other implementations might read them from a file or socket
+/// connection.
+/// 
+/// Every BitDecoder will have a BitSource from which it will read the data that
+/// it decodes.
 pub trait BitSource {
 
-    fn read(&self, dest: &[bool]);
+    /// Attempts to read bools from this source and put them into *dest*. 
+    /// 
+    /// The first bool read will be put in `dest[0]`, the second bool will be 
+    /// put into `dest[1]`, the third bool will be put into `dest[2]`... 
+    /// That will continue until *dest* has been filled completely. 
+    /// 
+    /// If enough bools could be read to fill *dest* completely, this method
+    /// will return `Ok`. If all bools were read before *dest* was filled or
+    /// if another error occurred while reading, a `ReadError` will be
+    /// returned.
+    fn read(&self, dest: &[bool]) -> Result<(),ReadError>;
 }
