086f20cbad8f73ed8079091fc18b51b211171f5e
knokko <knokogator@hotmail.com>
2020-11-02 13:28:04 +0100

    Finish documentation for now

--- src/lib.rs
+++ src/lib.rs
@@ -70,6 +70,21 @@ pub fn bools_to_bytes(bools: &[bool]) -> Vec<u8> {
     bytes
 }
 
+/// Converts a sequence of bytes to a sequence of booleans. Every byte will be
+/// converted to 8 booleans. This function is basically the opposite of
+/// *bools_to_bytes*: the result of *bools_to_bytes(bytes_to_bools(some_bytes))*
+/// will always be *some_bytes* again. See the documentation of *bools_to_bytes*
+/// for details. 
+/// 
+/// # Example
+/// ```
+/// use bit_encoding::bytes_to_bools;
+/// 
+/// assert_eq!(vec![true, true, false, true, false, false, false, false], bytes_to_bools(&[11]));
+/// ```
+/// (The example above encodes only 1 byte, but it is possible to convert more than 1
+/// byte at once. However, that would cause the above code snippet to get very long
+/// and hard to read.)
 pub fn bytes_to_bools(bytes: &[u8]) -> Vec<bool> {
     let num_bools = bytes.len() * 8;
     let mut bools = Vec::with_capacity(num_bools);
